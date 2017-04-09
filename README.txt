7f169d8312a66cb595bd1af3ddc2581620755307
mattjfan <mfan@umd.edu>
2017-04-09 02:30:45 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,17 @@
+Zencode README
+Copyright 2017 Matthew Fan
+Developed at Bitcamp 2017 with Matthew Fan and Rohan Dixit
+
+----------------------------------------------------------
+Q:	What is Zencode?
+A:	Zencode is a flexible programming language without a formal syntax which interprets psuedo-code-like, 
+	natural language into compilable python code (hopefully more languages coming soon).
+
+Q:	How	does Zencode work?
+A:	Simply type what you want Zencode to do. It identifies words that are designated as possible function words,
+	and extracts those into a list where it analyzes possible code blocks that can be comprised of the given functions
+	and arguments. In the case that there is ambiguity, Zencode can prompt the user for clarification.
+	
+Q:	Do you guys have a list of supported functions?
+A:	I'll let you know when we finish ;)
+
---
+++ alexa.py
@@ -0,0 +1,90 @@
+from compiler import Compiler
+from interpreter import Interpreter 
+
+# Import smtplib for the actual sending function
+import smtplib
+import time
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from email.mime.text import MIMEText
+
+import flask
+app = flask.Flask(__name__)
+DEBUG=True
+code=""
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+@app.route('/',methods=['GET', 'POST'])
+def main():
+	global code
+	if code=="":
+		hasCode=False;
+	else:
+		hasCode=True
+	if flask.request.method == 'POST':
+		print('it is posting')
+		if flask.request.form['submit']=='convert':
+			if flask.request.form['code']!="":
+				code=Compiler.runCompiler(flask.request.form['code'])
+			else:code=""
+		elif flask.request.form['submit']=='textMe':
+			sendMessage(message_text=code)
+			code=""
+		return flask.redirect('/redirect')
+	context={'code':code,'hasCode':hasCode}
+	return flask.render_template('mainpage.html', **context)
+	
+	
+@app.route('/redirect')
+def redirect():	
+	return flask.redirect('/')
+	
+	
+def sendMessage(message_text='No Message Content',target='2405852538@vtext.com'):
+	print(message_text)
+	message = MIMEText(message_text)
+	message['Date'] = formatdate()
+	message['From'] = "zencodehackathon@gmail"
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login("zencodehackathon@gmail.com", "bitcamp2017")
+	server.sendmail("zencodehackathon@gmail.com", "2405852538@vtext.com", message.as_string())
+	server.quit()
+"""
+myMessage=str(Compiler.runCompiler("Say Hello 6 times"))
+print(myMessage)
+sendMessage(myMessage)
+"""
+if __name__ == '__main__':
+	app.run(debug=DEBUG) 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
